9fb97ebaae045b14f6b017f74f22b5fda1dde3e7
Mark Toth <guzmotm@gmail.com>
2023-12-07 00:20:48 +0100

    Kodot tisztogattam

--- main/tempLoggerMain.c
+++ main/tempLoggerMain.c
@@ -39,177 +39,9 @@ static const char *TAG = "example";
 static char REQUEST[300];
 
 
-extern const uint8_t server_root_cert_pem_start[] asm("_binary_server_root_cert_pem_start");
-extern const uint8_t server_root_cert_pem_end[]   asm("_binary_server_root_cert_pem_end");
-
 
 void io_init();
 
-static void https_get_task(void *pvParameters)
-{
-    char buf[512];
-    int ret, len;
-
-    while(1) {
-        esp_tls_cfg_t cfg = {
-            .cacert_pem_buf  = server_root_cert_pem_start,
-            .cacert_pem_bytes = server_root_cert_pem_end - server_root_cert_pem_start,
-        };
-
-        struct esp_tls *tls = esp_tls_conn_http_new(WEB_URL, &cfg);
-
-        if(tls != NULL) {
-            ESP_LOGI(TAG, "Connection established...");
-        } else {
-            ESP_LOGE(TAG, "Connection failed...");
-            goto exit;
-        }
-
-        size_t written_bytes = 0;
-        do {
-            ret = esp_tls_conn_write(tls,
-                                     REQUEST + written_bytes,
-                                     strlen(REQUEST) - written_bytes);
-            if (ret >= 0) {
-                ESP_LOGI(TAG, "%d bytes written", ret);
-                written_bytes += ret;
-            } else if (ret != MBEDTLS_ERR_SSL_WANT_READ  && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
-                ESP_LOGE(TAG, "esp_tls_conn_write  returned 0x%x", ret);
-                goto exit;
-            }
-        } while(written_bytes < strlen(REQUEST));
-
-        ESP_LOGI(TAG, "Reading HTTP response...");
-
-        do
-        {
-            len = sizeof(buf) - 1;
-            bzero(buf, sizeof(buf));
-            ret = esp_tls_conn_read(tls, (char *)buf, len);
-
-            if(ret == MBEDTLS_ERR_SSL_WANT_WRITE  || ret == MBEDTLS_ERR_SSL_WANT_READ)
-                continue;
-
-            if(ret < 0)
-           {
-                ESP_LOGE(TAG, "esp_tls_conn_read  returned -0x%x", -ret);
-                break;
-            }
-
-            if(ret == 0)
-            {
-                ESP_LOGI(TAG, "connection closed");
-                break;
-            }
-
-            len = ret;
-            ESP_LOGD(TAG, "%d bytes read", len);
-            /* Print response directly to stdout as it is read */
-            for(int i = 0; i < len; i++) {
-                putchar(buf[i]);
-            }
-        } while(1);
-
-    exit:
-        esp_tls_conn_delete(tls);
-        putchar('\n'); // JSON output doesn't have a newline at end
-
-        static int request_count;
-        ESP_LOGI(TAG, "Completed %d requests", ++request_count);
-
-        for(int countdown = 10; countdown >= 0; countdown--) {
-            ESP_LOGI(TAG, "%d...", countdown);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        ESP_LOGI(TAG, "Starting again!");
-    }
-}
-
-
-static void http_get_task(void *pvParameters)
-{
-    const struct addrinfo hints = {
-        .ai_family = AF_INET,
-        .ai_socktype = SOCK_STREAM,
-    };
-    struct addrinfo *res;
-    struct in_addr *addr;
-    int s, r;
-    char recv_buf[64];
-
-    while(1) {
-        int err = getaddrinfo(WEB_SERVER, "80", &hints, &res);
-
-        if(err != 0 || res == NULL) {
-            ESP_LOGE(TAG, "DNS lookup failed err=%d res=%p", err, res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        /* Code to print the resolved IP.
-
-           Note: inet_ntoa is non-reentrant, look at ipaddr_ntoa_r for "real" code */
-        addr = &((struct sockaddr_in *)res->ai_addr)->sin_addr;
-        ESP_LOGI(TAG, "DNS lookup succeeded. IP=%s", inet_ntoa(*addr));
-
-        s = socket(res->ai_family, res->ai_socktype, 0);
-        if(s < 0) {
-            ESP_LOGE(TAG, "... Failed to allocate socket.");
-            freeaddrinfo(res);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... allocated socket");
-
-        if(connect(s, res->ai_addr, res->ai_addrlen) != 0) {
-            ESP_LOGE(TAG, "... socket connect failed errno=%d", errno);
-            close(s);
-            freeaddrinfo(res);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-
-        ESP_LOGI(TAG, "... connected");
-        freeaddrinfo(res);
-
-        if (write(s, REQUEST, strlen(REQUEST)) < 0) {
-            ESP_LOGE(TAG, "... socket send failed");
-            close(s);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... socket send success");
-
-        struct timeval receiving_timeout;
-        receiving_timeout.tv_sec = 5;
-        receiving_timeout.tv_usec = 0;
-        if (setsockopt(s, SOL_SOCKET, SO_RCVTIMEO, &receiving_timeout,
-                sizeof(receiving_timeout)) < 0) {
-            ESP_LOGE(TAG, "... failed to set socket receiving timeout");
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... set socket receiving timeout success");
-
-        /* Read HTTP response */
-        do {
-            bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
-            for(int i = 0; i < r; i++) {
-                putchar(recv_buf[i]);
-            }
-        } while(r > 0);
-
-        ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d.", r, errno);
-        close(s);
-        for(int countdown = 10; countdown >= 0; countdown--) {
-            ESP_LOGI(TAG, "%d... ", countdown);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-        }
-        ESP_LOGI(TAG, "Starting again!");
-    }
-}
 
 #define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
 #define NO_OF_SAMPLES   64          //Multisampling
@@ -270,7 +102,7 @@ void app_main()
 		ESP_ERROR_CHECK(esp_event_loop_create_default());
 		nvs_flash_init();
 		//tcpip_adapter_init();
-		wifi_init_softap();
+		//wifi_init_softap();
 
 		ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT,IP_EVENT_AP_STAIPASSIGNED, &connect_handler, &server));
 		/*while(1)
@@ -381,20 +213,13 @@ void app_main()
 				ssd1306_contrast(&dev, 0x1);
 				ssd1306_display_text_x3(&dev, 0, "Temp:", 4, false);
 				ssd1306_display_text_x3(&dev, 4, hofokString, strlen(hofokString), false);
-
-
-
-				  ESP_ERROR_CHECK( nvs_flash_init() );
-				    tcpip_adapter_init();
-
-				    //ESP_ERROR_CHECK(esp_event_loop_create_default());
-				    ESP_ERROR_CHECK(example_connect());
-
-
-            	sprintf(REQUEST, "GET " WEB_URL "%f HTTP/1.0\r\n" "Host: "WEB_SERVER"\r\n" "User-Agent: esp-idf/1.0 esp32\r\n""\r\n",((((temp-(temp_max+temp_min))/58))/1));
-
+			 // ESP_ERROR_CHECK( nvs_flash_init() );
+				tcpip_adapter_init();
+			    //ESP_ERROR_CHECK(esp_event_loop_create_default());
+			    ESP_ERROR_CHECK(example_connect());
+            	sprintf(REQUEST, "GET " WEB_URL "%f HTTP/1.0\r\n" "Host: "WEB_SERVER"\r\n" "User-Agent: esp-idf/1.0 esp32\r\n""\r\n",(hofok));
             	printf(REQUEST);
-            	    vTaskDelay(1000 /  portTICK_PERIOD_MS);
+            	//vTaskDelay(1000 /  portTICK_PERIOD_MS);
             	printf("Raw: %d\tVoltage: %f C\n", adc_reading, temp/60);
             	temp=0;
 
@@ -457,7 +282,7 @@ void app_main()
             	            bzero(recv_buf, sizeof(recv_buf));
             	            r = read(s, recv_buf, sizeof(recv_buf)-1);
             	            for(int i = 0; i < r; i++) {
-            	                putchar(recv_buf[i]);
+            	               // putchar(recv_buf[i]);
             	            }
             	        } while(r > 0);
 
